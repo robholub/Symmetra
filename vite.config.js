97c126a08a71afb070ba809e6fbd0961bd4e6b40
Rob <rob@robholub.com>
2026-04-21 10:02:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      // We set manifest to false because you already made a custom one in the public folder!
+      manifest: false, 
+      workbox: {
+        // This tells the service worker to securely cache all your code, styles, and images for offline use
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
 })
